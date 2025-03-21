be2c3e0eab535449ef912add07da72ae090a1279
pinkhydroflask <laurendanaho@gmail.com>
2025-03-21 10:41:07 +0800

    UserProfile page

--- src/router/Router.js
+++ src/router/Router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "@/components/Login.vue";  
 import LandingPage from "@/components/LandingPage.vue"; 
 import SignUp from "@/components/Signup.vue";
+import UserProfile from "@/components/UserProfile.vue";
 
 import Explore from "@/components/Explore.vue";
 import AddListing from "@/components/AddListing.vue";
@@ -11,7 +12,8 @@ const routes = [
   { path: "/Login", component: Login },
   { path: "/SignUp", component: SignUp },
   { path: "/Explore", component: Explore },
-  { path: '/add-listing', component: AddListing }
+  { path: '/add-listing', component: AddListing },
+  { path: "/UserProfile", component: UserProfile }
 ];
 
 const router = createRouter({
