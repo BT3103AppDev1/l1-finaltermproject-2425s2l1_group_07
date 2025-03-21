d56a150dd5f0daa1d20e1376f3f7549a62ed86ae
omgacai <134914115+omgacai@users.noreply.github.com>
2025-03-21 10:02:34 +0800

    routes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './index.js'; // Import router
+import router from './router/Router.js'; // Import router
+import './assets/global.css';
 
 const app = createApp(App);
 app.use(router); // Inject router globally
